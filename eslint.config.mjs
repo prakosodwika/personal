6d0795e5d0e63f7b018b47ee251c4b09ec16f3f6
Prakosodwika <prakosodwika4@gmail.com>
2025-10-05 00:07:38 +0800

    bug: mdx

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,7 @@ const eslintConfig = [
     "next/core-web-vitals",
     "next/typescript",
     "plugin:prettier/recommended",
-  "plugin:mdx/recommended"
+    "plugin:mdx/recommended"
   ),
   {
     rules: {
